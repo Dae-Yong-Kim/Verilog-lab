3c2c7ed52602904e118f91380e23627e2a42b3f3
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-10-14 16:06:40 +0900

    Using 7 Segment

---
+++ my_sw2LED/my_sw2LED.srcs/sources_1/new/my_sw2LED.v
@@ -0,0 +1,44 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/10/14 14:14:24
+// Design Name: 
+// Module Name: my_sw2LED
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_sw2LED(
+    input [3:0] SW,
+    output reg [7:0] LED
+    );
+    
+always @(SW)
+begin
+    case (SW)
+        4'b0000: LED  =8'h7e;
+        4'b0001: LED = 8'h30;
+        4'b0010: LED = 8'h6d;
+        4'b0011: LED = 8'h79;
+        4'b0100: LED = 8'h33;
+        4'b0101: LED = 8'h5b;
+        4'b0110: LED = 8'h5f;
+        4'b0111: LED = 8'h70;
+        4'b1000: LED = 8'h7f;
+        4'b1001: LED = 8'h7b;
+        default: LED = 8'hxx;
+    endcase
+end
+endmodule
