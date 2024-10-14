10bd87d0b02fe42b0d6ea850c253d6ff3df62e49
Dae-Yong-Kim <youbinlove607@gmail.com>
2024-10-14 12:15:25 +0900

    2bit Multiplexer

---
+++ my_mult/my_mult.srcs/sources_1/new/my_mult.v
@@ -0,0 +1,40 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 2024/10/14 11:15:48
+// Design Name: 
+// Module Name: my_mult
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module my_mult(
+    input a,
+    input b,
+    input [1:0] sel,
+    output reg z
+    );
+    
+always @(a, b, sel)
+begin
+    case (sel)
+        2'b00: z = ~a;
+        2'b01: z = ~(a & b);
+        2'b10: z = ~(a & (~b));
+        2'b11: z = (a ^ b);
+        default: z = 1'bx;
+    endcase
+end
+endmodule
